7a626c8b344456dfe6987871eb26cfb505cb5fba
Andre Tavares <andre.tavares@prodam.am.gov.br>
2021-04-29 08:22:22 -0400

    using v-else

--- 01 Intro to Vuejs 2/lesson 3A Starting Code/main.js
+++ 01 Intro to Vuejs 2/lesson 3A Starting Code/main.js
@@ -5,6 +5,6 @@ var app = new Vue({
      image: 'socks.jpg',
      altText: 'A pair of socks',
      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-     inStock: true
+     inventory: 100
    }
 })

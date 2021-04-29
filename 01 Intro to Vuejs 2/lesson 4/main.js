fa69e77533b63a8b6725a769ffcddc19fc528bae
Andre Tavares <andre.tavares@prodam.am.gov.br>
2021-04-29 10:33:27 -0400

    added iteration over data lists and objects

--- 01 Intro to Vuejs 2/lesson 4/main.js
+++ 01 Intro to Vuejs 2/lesson 4/main.js
@@ -7,7 +7,17 @@ var app = new Vue({
      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
      onSale: true,
      outStock: true,
-     details: ["80% cotton", "20% poliester", "Gender-neutral"]
+     details: ["80% cotton", "20% poliester", "Gender-neutral"],
+     variants: [
+       {
+         variantId:2234,
+         variantColor: 'green'
+       },
+       {
+        variantId:2235,
+        variantColor: 'blue'
+       }
+     ]
    }
 })
 

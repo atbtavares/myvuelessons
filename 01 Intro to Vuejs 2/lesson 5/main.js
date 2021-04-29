9d55bfd7b328072fc6f3e6a81b198c1c032cea1b
Andre Tavares <andre.tavares@prodam.am.gov.br>
2021-04-29 11:57:02 -0400

    added button and cart

---
+++ 01 Intro to Vuejs 2/lesson 5/main.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+   el: '#app',
+   data: {
+     product: 'Socks',
+     image: 'socks.jpg',
+     altText: 'A pair of socks',
+     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+     onSale: true,
+     outStock: true,
+     details: ["80% cotton", "20% poliester", "Gender-neutral"],
+     variants: [
+       {
+         variantId:2234,
+         variantColor: 'green'
+       },
+       {
+        variantId:2235,
+        variantColor: 'blue'
+       }
+     ],
+     sizes: [
+        {
+          sizeId: 2235,
+          sizeDescription: "Small"
+        }, 
+        {
+          sizeId: 2236,
+          sizeDescription:"Medium"
+        },
+        {
+          sizeId: 2237,
+          sizeDescription: "Large"
+        }, 
+        {
+          sizeId: 2238,
+          sizeDescription:"XL"
+        }
+      ],
+      cart: 0
+   }
+})
+

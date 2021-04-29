12cf5bfba335e890a1f4adf9e1a6629bdf9f1afc
Andre Tavares <andre.tavares@prodam.am.gov.br>
2021-04-29 10:25:23 -0400

    lesson 4

---
+++ 01 Intro to Vuejs 2/lesson 4/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+   el: '#app',
+   data: {
+     product: 'Socks',
+     image: 'socks.jpg',
+     altText: 'A pair of socks',
+     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+     onSale: true,
+     outStock: true,
+     details: ["80% cotton", "20% poliester", "Gender-neutral"]
+   }
+})
+
